e36753918e1f9c147008758b3996c59864ea5799
Ondrej <lamorak@outlook.com>
2020-08-23 15:46:29 +0200

    Implement adding a cat to favourites

--- app/src/main/java/cz/lamorak/koti/detail/DetailFragment.kt
+++ app/src/main/java/cz/lamorak/koti/detail/DetailFragment.kt
@@ -2,15 +2,26 @@ package cz.lamorak.koti.detail
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import coil.load
 import cz.lamorak.koti.Cat
 import cz.lamorak.koti.R
+import cz.lamorak.koti.favourites.FavouritesDao
+import cz.lamorak.koti.favourites.model.FavouriteCat
 import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.koin.android.ext.android.inject
+import java.time.Instant
 
 class DetailFragment(cat: Cat) : Fragment(R.layout.fragment_detail) {
 
+    private val favouritesDao by inject<FavouritesDao>()
     private lateinit var catId: String
     private lateinit var imageUrl: String
 
@@ -32,6 +43,25 @@ class DetailFragment(cat: Cat) : Fragment(R.layout.fragment_detail) {
         cat_image.load(imageUrl)
         cat_image.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+        favourite.setOnClickListener {
+            favourite.hide()
+            addCatToFavourites()
+        }
+    }
+
+    private fun addCatToFavourites() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val cat = FavouriteCat(catId, imageUrl, Instant.now())
+            favouritesDao.insertFavoutite(cat)
+            showSuccessMessage()
+        }
+    }
+
+    fun showSuccessMessage() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            Toast.makeText(requireContext(), "Cat added to favourites!", Toast.LENGTH_SHORT)
+                    .show()
         }
     }
 
